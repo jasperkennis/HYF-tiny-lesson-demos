aae8a072acd92f606a9335ebe370853ad4b8bd60
Jasper Kennis <jasper.kennis@mollie.com>
2021-01-31 10:47:53 +0100

    Initial commit

---
+++ teaching-c30-js1-w1/broken-name-example.js
@@ -0,0 +1,11 @@
+const generalGreeting = 'Hello everyone!'
+const personalGreeting = `Hello ${name}!`
+
+const students = [
+  'Ben',
+  'Sarah'
+]
+
+students.forEach((name) => {
+  console.log(personalGreeting)
+})
