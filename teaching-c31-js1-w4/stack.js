c8a6978a2409296f907405061daef2f2514dbf4e
Frank Parejo <francisco.parejo.lopez@gmail.com>
2021-02-21 11:43:03 +0100

    add stack example.

---
+++ teaching-c31-js1-w4/stack.js
@@ -0,0 +1,27 @@
+const stack = [];
+
+// Push items to the stack
+stack.push(1);
+console.log(stack);
+
+stack.push(2);
+console.log(stack);
+
+stack.push(3);
+console.log(stack);
+
+stack.push(4);
+console.log(stack);
+
+// Pop items from the stack
+stack.pop();
+console.log(stack);
+
+stack.pop();
+console.log(stack);
+
+stack.pop();
+console.log(stack);
+
+stack.pop();
+console.log(stack);
