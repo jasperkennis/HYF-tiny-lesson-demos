77d75be9d5e65a246315e56536d8d05c3969eabc
Jasper Kennis <jasper.kennis@mollie.com>
2021-02-21 10:40:15 +0100

    too difficult

--- teaching-c31-js1-w4/observer.js
+++ teaching-c31-js1-w4/observer.js
@@ -1,3 +1,29 @@
 const objThatWillChange = {
-  lala: 'abc'
+  internals: {
+    lala: 'abc'
+  },
+
+  // Exposed variable
+  lala: 'abc',
+
+  // Setter
+  set lala(val) {
+    // Call listener
+    this.hardcodedListener()
+
+    // Set internal value
+    this.internals.lala = val
+  },
+
+  // Getter
+  get lala() {
+    return this.internals.lala
+  },
+
+  // Listener
+  hardcodedListener() {
+    console.log('a listener triggered')
+  }
 }
+
+objThatWillChange.lala = 123
